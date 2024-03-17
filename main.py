cfed5cb06367418bf0d5d178f450edb4f7ca14bc
Gookuruto <gokuruto1234@gmail.com>
2024-03-17 10:34:22 +0100

    Propsty kalkulator

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 print("To jest testowe repozytorium z testową wiadomością.")
 
 print("test dev")
+
+
+print("Witaj w kalkulatorze")
+
+a = int(input("Podaj pierwsza liczbe: "))
+b = int(input("podaj druga liczbe: "))
+sign = input("podaj działanie: ")
+
+if sign == "+":
+    print(a+b)
+if sign=="-":
+    print(a-b)
+if sign=="*":
+    print(a*b)
+if sign=="/":
+    print(a/b)
+print("Koniec programu")
